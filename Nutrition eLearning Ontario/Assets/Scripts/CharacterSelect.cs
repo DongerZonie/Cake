cd002b25b837069c5b335d7a662d1a5f421db9b2
unknown <kieran.ramnarine@ryerson.ca>
2018-03-02 12:36:20 -0500

    Updated script. Fixed metrics. Started character select screen.

---
+++ Nutrition eLearning Ontario/Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelect : MonoBehaviour {
+
+    public Image character;
+    public Sprite thisOne;
+
+    public void Highlight()
+    {
+        print("Mouse is over " + gameObject.name);
+    }
+    public void RemoveHighlight()
+    {
+        print("Mouse is not over " + gameObject.name);
+    }
+
+    public void SetCharacter()
+    {
+        character.sprite = thisOne;
+    }
+}
