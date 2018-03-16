c55d32a154536343804192a6fe39f466d540fa23
Kieran Ramnarine <kieran.ramnarine@ryerson.ca>
2018-03-16 11:47:16 -0400

    Fixed broken links, added photos, added backgrounds to Saad Story

---
+++ Nutrition eLearning Ontario/Assets/Scripts/BackgroundChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Yarn.Unity;
+using UnityEngine.UI;
+
+public class BackgroundChange : MonoBehaviour {
+
+    string sceneImageFolder;
+    public Sprite background;
+
+	// Use this for initialization
+	void Start () {
+        sceneImageFolder = SceneManager.GetActiveScene().name;
+	}
+    
+    [YarnCommand("changeBackground")]
+    public void Change(string file)
+    {
+        print("Images/Backgrounds/" + sceneImageFolder + "/" + file);
+        background = Resources.Load<Sprite>("Images/Backgrounds/" + sceneImageFolder + "/" + file);
+        GetComponent<Image>().sprite = background;
+    }
+}
